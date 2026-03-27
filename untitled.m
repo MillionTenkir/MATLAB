8a27e08f776f7dc703161ab1d885d582c1c3431e
MillionTenkir <million@cheche.et>
2026-03-27 11:29:27 +0100

    Add simple operations to tables

--- untitled.m
+++ untitled.m
@@ -1,1 +1,9 @@
-X=readtable("filename.xls","Sheet","Sheet1","ReadRowNames",true, "ReadVariableNames",true)
+X = readtable("filename.xls","Sheet","Sheet1","ReadRowNames",true,"ReadVariableNames",true);
+
+bool1=(string(X.Gender))=="Female";
+bool2=X.Age>=25 & X.Age<=40;
+bool3 = bool1 & bool2;
+resultTable = X(bool3, {'Id', 'Gender', 'Age'});
+
+bool3=bool1 & bool2
+[X.Id bool1 bool2 bool3]
